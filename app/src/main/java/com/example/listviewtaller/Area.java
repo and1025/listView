b5d25415de1e34b5537bd694804b84472a8bd98a
developer <developer@MacBook-Air-de-Andres.local>
2020-06-15 00:33:35 -0500

    creación del primer listView

---
+++ app/src/main/java/com/example/listviewtaller/Area.java
@@ -0,0 +1,17 @@
+package com.example.listviewtaller;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Area extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_area);
+    }
+}
